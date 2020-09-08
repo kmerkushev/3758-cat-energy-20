ce2c2bb72a369a4d175fb9c5bbe083614b37456a
kmerkushev <regesc@yandex.ru>
2020-09-08 12:22:22 +0300

    7. Продолжаем вёрстку с препроцессором

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,10 @@
+
+
 let menu = document.getElementsByClassName('main-nav')[0];
 let btnMenu = document.getElementsByClassName('header__toggle')[0];
 
+menu.classList.remove("header__main-nav--nojs");
+menu.classList.add("visually-hidden");
 
 btnMenu.addEventListener("click", toggleMenu);
 
